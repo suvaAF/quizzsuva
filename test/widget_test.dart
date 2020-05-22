f40391029bd225b223e4139c77a902008b5e2cde
Suva <schea1979@hotmail.fr>
2020-05-23 01:05:22 +0200

    update phase 2 quizz

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quizzsuva/widgets/mon_app.dart';
 
 import 'package:quizzsuva/main.dart';
 
