42a9d74962ad1f4e7fae66e6631b9997aa9791c1
Suva <schea1979@hotmail.fr>
2020-05-23 17:54:38 +0200

    Update accueil et custom

--- lib/widgets/accueil.dart
+++ lib/widgets/accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzsuva/widgets/custom_text.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -18,8 +19,25 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
+            new Card(
+              elevation: 10.0,
+              child: new Container(
+                height: MediaQuery.of(context).size.width * 0.8,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: new Image.asset("quizz assets/cover.jpg",
+                    fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            new RaisedButton(
+              color: Colors.blue,
+                onPressed: () {
+                  
+                },
+              child: new CustomText("Commencer le Quizz", factor: 1.5),
+            )
           ],
         ),
       ),
